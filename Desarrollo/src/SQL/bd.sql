58197b3a47c0af3f855c9dd66231065256a6d813
edisonmamian <edison.mamian@correounivalle.edu.co>
2016-01-09 17:19:56 -0500

    operador

--- Desarrollo/src/SQL/bd.sql
+++ Desarrollo/src/SQL/bd.sql
@@ -1,7 +1,7 @@
 DROP SEQUENCE IF EXISTS identificador CASCADE;
 DROP TABLE IF EXISTS sedes;
 DROP TABLE IF EXISTS usuarios;
-
+DROP TABLE IF EXISTS pqr;
 
 CREATE TABLE sedes (
 	nombre VARCHAR(40),
@@ -28,23 +28,19 @@ CREATE SEQUENCE identificador
 START 100
 increment by 1;
 
-DROP TABLE IF EXISTS pqr;
-
 CREATE TABLE pqr (
-
-id_pqr INTEGER  DEFAULT nextval('identificador') NOT NULL,
-tipo_pqr VARCHAR(25) NOT NULL,
-detalle VARCHAR(200) NOT NULL,
-nombre VARCHAR(50) NOT NULL,
-apellido VARCHAR(100) NOT NULL,
-cedula VARCHAR(15) NOT NULL,
-direccion VARCHAR(50) NOT NULL,
-telefono VARCHAR(10) NOT NULL,
-email VARCHAR(50) NOT NULL,
-sede VARCHAR(50) NOT NULL,
-estado VARCHAR(30) NOT NULL,
-respuesta VARCHAR(200),
-PRIMARY KEY(id_pqr)
-
+        id_pqr INTEGER  DEFAULT nextval('identificador') NOT NULL,
+        tipo_pqr VARCHAR(25) NOT NULL,
+        detalle VARCHAR(200) NOT NULL,
+        nombre VARCHAR(50) NOT NULL,
+        apellido VARCHAR(100) NOT NULL,
+        cedula VARCHAR(15) NOT NULL,
+        direccion VARCHAR(50) NOT NULL,
+        telefono VARCHAR(10) NOT NULL,
+        email VARCHAR(50) NOT NULL,
+        sede VARCHAR(50) NOT NULL,
+        estado VARCHAR(30) NOT NULL,
+        respuesta VARCHAR(200),
+        PRIMARY KEY(id_pqr)
 );
 
